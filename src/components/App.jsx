98752e6d1d630ae59d6d2809fe2b530239e3505d
ViktoriiaYakaba <yakaba@bk.ru>
2024-03-14 16:27:33 +0100

    summary

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,21 +4,11 @@ import Description from "./Description";
 import Options from "./Options";
 import Feedback from "./Feedback";
 
-
 const App = () => {
-  const [feedback, setFeedback] = useState({
-    good: 0,
-    neutral: 0,
-    bad: 0
-  });
-
-  useEffect(() => {
+  const [feedback, setFeedback] = useState(() => {
     const savedFeedback = JSON.parse(localStorage.getItem('feedback'));
-    if (savedFeedback) {
-      setFeedback(savedFeedback);
-    }
-  }, []);
-
+    return savedFeedback ? savedFeedback : { good: 0, neutral: 0, bad: 0 };
+  });
 
   const handleFeedback = (type) => {
     setFeedback((prevFeedback) => ({
@@ -28,12 +18,12 @@ const App = () => {
   };
 
   const handleReset = () => {
-    setFeedback({
-      good: 0,
-      neutral: 0,
-      bad: 0
-    });
+    setFeedback({ good: 0, neutral: 0, bad: 0 });
   };
+
+  useEffect(() => {
+    localStorage.setItem('feedback', JSON.stringify(feedback));
+  }, [feedback]);
 
   const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
   const positivePercentage = Math.round(((feedback.good + feedback.neutral) / totalFeedback) * 100);
@@ -42,17 +32,21 @@ const App = () => {
     <div className="container">
       <Description />
       <Options onFeedback={handleFeedback} onReset={handleReset} hasFeedback={totalFeedback > 0} />
-      {totalFeedback > 0 && (
-      <Feedback
+      {totalFeedback > 0 ? (
+        <Feedback
           good={feedback.good}
           neutral={feedback.neutral}
           bad={feedback.bad}
           total={totalFeedback}
           positivePercentage={positivePercentage}
         />
+      ) : (
+        <p>No feedback yet</p>
       )}
     </div>
   );
 };
 
 export default App;
+
+
--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -1,20 +1,13 @@
 
-
 const Feedback = ({ good, neutral, bad, total, positivePercentage }) => {
     return ( 
     <div>
-        {total > 0 ? (
-            <div>
-                <h2>Feedback Statistics</h2>
-                <p>Good: {good}</p>
-                <p>Neutral: {neutral}</p>
-                <p>Bad: {bad}</p>
-                <p>Total: {total}</p>
-                <p>Positive Percentage: {positivePercentage}%</p>
-            </div>
-        ) : (
-            <p>No feedback yet</p>
-        )}
+        <h2>Feedback Statistics</h2>
+        <p>Good: {good}</p>
+        <p>Neutral: {neutral}</p>
+        <p>Bad: {bad}</p>
+        <p>Total: {total}</p>
+        <p>Positive Percentage: {positivePercentage}%</p>
     </div>)
 };
 export default Feedback;
